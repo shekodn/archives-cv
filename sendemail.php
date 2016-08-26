24fe0a6aacf8740e2e35c1f9646d7c20f28c3e87
shekodn <sdiazn19@gmail.com>
2016-08-26 14:36:54 -0500

    my photo

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'sdiazn10@gmail.com';  // put your email here
+    $to         = 'sdiazn19@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
